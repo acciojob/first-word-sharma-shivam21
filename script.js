2d3f105dcbefb225e55db7fe08e17038599c2836
sharma-shivam21 <s3860762@gmail.com>
2025-06-02 21:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
-  // your code here
+  if (!s) return ''; // If the string is empty
+  s = s.trim(); // Remove leading/trailing spaces
+  const index = s.indexOf(' '); // Find the first space
+
+  if (index === -1) {
+    return s; // No space found, return the whole string
+  }
+
+  return s.substring(0, index); // Return substring up to first space
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
